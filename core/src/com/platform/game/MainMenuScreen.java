ec987f1261c6e10d1cc90fb46771311ab0ba328f
Tim Peck <70923280+pecktj@users.noreply.github.com>
2024-04-12 10:38:21 -0400

    added start screen

--- core/src/com/platform/game/MainMenuScreen.java
+++ core/src/com/platform/game/MainMenuScreen.java
@@ -3,6 +3,7 @@ package com.platform.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MainMenuScreen implements Screen {
@@ -22,11 +23,12 @@ public class MainMenuScreen implements Screen {
     @Override
     public void render(float delta) {
         ScreenUtils.clear(0, 0, 0.2f, 1);
-
+        Texture start = new Texture(Gdx.files.internal("background/start screen.png"));
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
 
         game.batch.begin();
+        game.batch.draw(start, 0, 0, 2400, 1150);
         game.font.draw(game.batch, "Welcome!!! ", (float) game.getWidth() / 2 - 100, (float) game.getHeight() / 2);
         game.font.draw(game.batch, "Tap anywhere to begin!", (float) game.getWidth() / 2 - 100, (float) game.getHeight() / 2 - 50);
         game.batch.end();
